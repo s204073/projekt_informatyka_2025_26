82d57308906e0c60f7929cc141fd12e1634982e7
s204073 <maciek4740@gmail.com>
2025-12-07 15:14:14 +0100

    Add main2.cpp to initialize and run Game

---
+++ rep5/main2.cpp
@@ -0,0 +1,16 @@
+//main.cpp
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Game.h"			//tu zawarte include innych klas
+
+int main() {
+
+	Game game;		//klasa Game, obiekt game - jedna pojedyncza rozgrywka
+	game.run();
+	return 0;
+
+}
+
+
+
+
